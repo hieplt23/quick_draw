3195e4c7b3807b6141ccbf78b4a368005a9a6599
Hiep Le Thanh <149412519+hieplt23@users.noreply.github.com>
2024-08-01 13:59:01 +0700

    Update train.py

--- train.py
+++ train.py
@@ -14,19 +14,15 @@ from src.utils import get_evaluation
 
 
 def get_args():
-    parser = argparse.ArgumentParser(
-        """Implementation of the Quick Draw model proposed by Google""")
+    parser = argparse.ArgumentParser("""Implementation of the Quick Draw model proposed by Google""")
     parser.add_argument("--optimizer", type=str, choices=["sgd", "adam"], default="sgd")
     parser.add_argument("--total_images_per_class", type=int, default=10000)
     parser.add_argument("--ratio", type=float, default=0.8, help="the ratio between training and test sets")
     parser.add_argument("--batch_size", type=int, default=32)
     parser.add_argument("--num_epochs", type=int, default=10)
-    parser.add_argument("--lr", type=float,
-                        default=0.01)  # recommended learning rate for sgd is 0.01, while for adam is 0.001
-    parser.add_argument("--es_min_delta", type=float, default=0.0,
-                        help="Early stopping's parameter: minimum change loss to qualify as an improvement")
-    parser.add_argument("--es_patience", type=int, default=3,
-                        help="Early stopping's parameter: number of epochs with no improvement after which training will be stopped. Set to 0 to disable this technique.")
+    parser.add_argument("--lr", type=float, default=0.01)  # recommended learning rate for sgd is 0.01, while for adam is 0.001
+    parser.add_argument("--es_min_delta", type=float, default=0.0, help="Early stopping's parameter: minimum change loss to qualify as an improvement")
+    parser.add_argument("--es_patience", type=int, default=3, help="Early stopping's parameter: number of epochs with no improvement after which training will be stopped. Set to 0 to disable this technique.")
     parser.add_argument("--data_path", type=str, default="data", help="the root folder of dataset")
     parser.add_argument("--log_path", type=str, default="tensorboard")
     parser.add_argument("--saved_path", type=str, default="trained_models")
